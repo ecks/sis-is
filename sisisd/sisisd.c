43407de1a09df79631253158584474c1b5bdb164
Stephen Sigwart <ssigwart@ssigwart-mac.local>
2011-01-21 15:39:56 -0500

    SIS-IS

--- sisisd/sisisd.c
+++ sisisd/sisisd.c
@@ -150,7 +150,7 @@ static int sisis_recvfrom(struct thread *thread)
 	char buf[1024];
 	memset (buf, 0, 1024);
 	int recv_len;
-	int fromlen = sizeof from;
+	int from_len = sizeof from;
 	recv_len = recvfrom(sisis_sock, buf, 1024, 0, &from, &from_len);
 	if (recv_len < 0)
 	{
