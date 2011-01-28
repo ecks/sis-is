be8cb6daca37ce04cf636bb565f3bb3718db2d9c
Stephen Sigwart <ssigwart@gmail.com>
2011-01-27 19:09:40 -0500

    Cleaning up todos.  Adding ACKs and timeouts.

--- sisis_api.c
+++ sisis_api.c
@@ -22,8 +22,7 @@ struct sockaddr_in sisis_listener_addr;
 
 int sisis_listener_port = 54345;
 char * sisis_listener_ip_addr = "127.0.0.1";
-srand((int)time(NULL));
-unsigned int next_request_id = (unsigned int) rand();
+unsigned int next_request_id = 1;
 
 // TODO: Support multiple addresses at once.
 pthread_t sisis_reregistration_thread;
