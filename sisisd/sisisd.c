dcdf8ee11e8653ad7f1676d23b83b841ff16ae9c
Stephen Sigwart <ssigwart@gmail.com>
2011-01-27 19:58:59 -0500

    Cleaning up todos.  Adding ACKs and timeouts.

--- sisisd/sisisd.c
+++ sisisd/sisisd.c
@@ -153,8 +153,8 @@ void sisis_process_message(char * msg, int msg_len, int sock, struct sockaddr * 
 					{
 						// Construct reply
 						char * buf;
-						sisis_construct_message(&buf, SISIS_MESSAGE_VERSION, request_id, SISIS_NACK, NULL, 0);
-						sendto(sock, buf, strlen(buf), 0, from, from_len);
+						int buf_len = sisis_construct_message(&buf, SISIS_MESSAGE_VERSION, request_id, SISIS_NACK, NULL, 0);
+						sendto(sock, buf, buf_len, 0, from, from_len);
 						free(buf);
 						
 						zlog_err ("sisis_process_message: Invalid SIS-IS address: %s", ip_addr);
@@ -166,9 +166,9 @@ void sisis_process_message(char * msg, int msg_len, int sock, struct sockaddr * 
 					
 					// Construct reply
 					char * buf;
-					sisis_construct_message(&buf, SISIS_MESSAGE_VERSION, request_id, (status == 0) ? SISIS_ACK : SISIS_NACK, NULL, 0);
+					int buf_len = sisis_construct_message(&buf, SISIS_MESSAGE_VERSION, request_id, (status == 0) ? SISIS_ACK : SISIS_NACK, NULL, 0);
 					printf("\tSending %s\n", (status == 0) ? "ACK" : "NACK");
-					sendto(sock, buf, strlen(buf), 0, from, from_len);
+					sendto(sock, buf, buf_len, 0, from, from_len);
 					free(buf);
 				}
 				break;
