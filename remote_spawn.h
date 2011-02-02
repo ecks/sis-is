081e8682be4445b5033a71afd70fcda027a36871
Stephen Sigwart <ssigwart@gmail.com>
2011-02-02 17:15:12 -0500

    Remote Spawn.

--- remote_spawn.h
+++ remote_spawn.h
@@ -5,8 +5,8 @@
 
 // Requests
 #define REMOTE_SPAWN_REQ_START									0
-#define REMOTE_SPAWN_REQ_STOP										1
-#define REMOTE_SPAWN_REQ_RESTART								2
+//#define REMOTE_SPAWN_REQ_STOP										1	// Probably not needed, we won't know which one to stop if multiple processes exist
+//#define REMOTE_SPAWN_REQ_RESTART								2 // Probably not needed, we won't know which one to stop if multiple processes exist
 
 // Responses
 #define REMOTE_SPAWN_RESP_OK										0
