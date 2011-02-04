d2c378540d2545ea87343de19735ce8372dde033
Stephen Sigwart <ssigwart@gmail.com>
2011-02-04 11:24:42 -0500

    Remote spawn process data file.

--- remote_spawn.c
+++ remote_spawn.c
@@ -195,8 +195,8 @@ int main (int argc, char ** argv)
 #define PROCS_DAT_PARSE_LINE_FOUND_ARG1					0x00000008
 #define PROCS_DAT_PARSE_LINE_ESCAPE_SEQ_STARTED	0x00000010
 #define PROCS_DAT_PARSE_LINE_DONE								0x00000020
-#define PROCS_DAT_PARSE_LINE_ALL								0x0000003e
 #define PROCS_DAT_PARSE_LINE_ERROR							0x00000040
+#define PROCS_DAT_PARSE_LINE_ALL								0x0000007e
 				// Read in line from file
 				char line[1024];
 				int proc_dat_parse_flags = 0, linenum = 1;
@@ -242,13 +242,13 @@ int main (int argc, char ** argv)
 								proc_dat_parse_flags |= PROCS_DAT_PARSE_LINE_ERROR;
 							}
 						}
-						// Parsing path
-						else if (!(proc_dat_parse_flags & (PROCS_DAT_PARSE_LINE_FOUND_PATH | PROCS_DAT_PARSE_LINE_FOUND_ARG1)))
+						// Parsing path and arg1
+						else if (!(proc_dat_parse_flags & PROCS_DAT_PARSE_LINE_FOUND_PATH) || !(proc_dat_parse_flags & PROCS_DAT_PARSE_LINE_FOUND_ARG1)))
 						{
 							// We need starting quote
 							if (str[0] == '\0' && line[i] != '"')
 							{
-								printf("3\n");
+								printf("3 %d\n", i);
 								proc_dat_parse_flags |= PROCS_DAT_PARSE_LINE_ERROR;
 							}
 							// Starting string
