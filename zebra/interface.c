8506977757a4309c3c11f61d2dd195aac198e49a
Stephen Sigwart <ssigwart@gmail.com>
2011-04-19 15:12:31 -0400

    Trying to remove IPv6 Reject Route on interface address add.

--- zebra/interface.c
+++ zebra/interface.c
@@ -1458,6 +1458,7 @@ ipv6_address_install (struct vty *vty, struct interface *ifp,
 		//netlink_del_reject_route(AF_INET6, &p->prefix, p->prefixlen, if_lookup_by_name("lo"));
 		//netlink_del_reject_route(AF_INET6, &p->prefix, p->prefixlen, lo_index);
 		int lo_index = if_lookup_by_name("lo");
+		zlog_debug ("Remove reject route: %d %d %d", ifp->ifindex == lo_index, ifp->ifindex, lo_index);
 		if (ifp->ifindex == lo_index)
 			netlink_del_reject_route(AF_INET6, &p->prefix, 128, lo_index);
 
