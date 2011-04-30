de7bb078fd9a17dd3ece5cbdb654501f39cec624
Stephen Sigwart <ssigwart@gmail.com>
2011-04-29 22:05:54 -0400

    Demo process.

--- shim.c
+++ shim.c
@@ -59,6 +59,8 @@ int main (int argc, char ** argv)
 	// Get pid
 	pid = getpid();
 	
+	printf("Opening socket...\n");
+	
 	// Set up socket address info
 	struct addrinfo hints, *addr;
 	memset(&hints, 0, sizeof hints);
