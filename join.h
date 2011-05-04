35715af08c801828c60c7b82434cd58de2645520
Stephen Sigwart <ssigwart@gmail.com>
2011-05-04 17:14:06 -0400

    Simple voter for demo program.

--- join.h
+++ join.h
@@ -12,6 +12,19 @@
 
 #define GATHER_RESULTS_TIMEOUT_USEC		100000	// 100ms
 
+// From time.h
+#ifndef timersub
+#define timersub(a, b, result)						      \
+  do {									      \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;			      \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;			      \
+    if ((result)->tv_usec < 0) {					      \
+      --(result)->tv_sec;						      \
+      (result)->tv_usec += 1000000;					      \
+    }									      \
+  } while (0)
+#endif
+
 #define MAX_TABLE_SIZE 50
 
 /** Count number of sort processes */
