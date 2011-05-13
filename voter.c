03d6cb7fef0a3405c8fb38c84d5623c7e60f2510
Stephen Sigwart <ssigwart@gmail.com>
2011-05-13 17:10:41 -0400

    Demo program: Visualization.

--- voter.c
+++ voter.c
@@ -37,6 +37,9 @@ table_group_item_t * cur_merge_table_item;
 // Expected table
 demo_merge_table_entry expected_table[MAX_TABLE_SIZE];
 int expected_table_size = 0;
+struct timeval expected_table_received;
+int expected_table_checked;
+pthread_mutex_t expected_table_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main (int argc, char ** argv)
 {
@@ -73,7 +76,11 @@ void * validator(void * param)
 		{
 			// Deserialize
 			int bytes_used;
+			pthread_mutex_lock(&expected_table_mutex);
+			gettimeofday(&expected_table_received, NULL);
 			expected_table_size = deserialize_join_table(expected_table, MAX_TABLE_SIZE, buf, buflen, &bytes_used);
+			expected_table_checked = 0;
+			pthread_mutex_unlock(&expected_table_mutex);
 		}
 	}
 	
@@ -127,12 +134,20 @@ void vote_and_process()
 		else
 		{
 			demo_merge_table_entry * join_table = (demo_merge_table_entry *)merge_table_item->table;
+			
+			// Get current time
+			struct timeval tv_now, tv_diff;
+			gettimeofday(&tv_now, NULL);
+			
 			int i;
 			/*
 			printf("Joined Rows: %d\n", merge_table_item->table_size);
 			for (i = 0; i < merge_table_item->table_size; i++)
 				printf("User Id: %d\tName: %s\tGender: %c\n", join_table[i].user_id, join_table[i].name, join_table[i].gender);
 			*/
+			pthread_mutex_lock(&expected_table_mutex);
+			// Determine time that passed
+			timersub(&tv_now, &expected_table_received, &tv_diff);
 			// Compare against expected table
 			short correct = 1;
 			if (expected_table_size != merge_table_item->table_size)
@@ -146,6 +161,11 @@ void vote_and_process()
 					if (join_table[i].user_id != expected_table[i].user_id || strcmp(join_table[i].name, expected_table[i].name) != 0 || join_table[i].gender != expected_table[i].gender)
 						correct = 0;
 			}
+			expected_table_checked = 1;
+			pthread_mutex_unlock(&expected_table_mutex);
+			
+			// Print timestamp
+			printf("%llu.%06llu: ", (uint64_t)tv_diff.tv_sec, (uint64_t)tv_diff.tv_usec);
 			
 			// Was this correct
 			if (correct)
