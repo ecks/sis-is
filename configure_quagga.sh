7aca4cc2ef193e63a2fde0f5f1e3b58093e3449a
Hristo Asenov <hristo.s.asenov@gmail.com>
2012-10-19 12:11:03 -0400

    added configuration files

---
+++ configure_quagga.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./configure --enable-sisisd --enable-rib-comp --enable-vtysh --enable-isisd --disable-pie --prefix=/usr --libexecdir=/usr/lib --libdir=/usr/lib --sysconfdir=/etc/quagga --sbindir=/usr/lib/quagga --localstatedir=/var/run/quagga
